b28e9ef53d7379c621468a941f528885631316b3
Andres <acpedrozab@ul.edu.co>
2024-10-10 21:51:18 -0300

    Archivos añadidos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from "react";
+import Header from "./componentes/Header";
+import Footer from "./componentes/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./componentes/Login";
+import Register from "./componentes/Register";
+import Cart from "./componentes/Cart";
+import ProductDetail from "./pages/ProductDetail";
+import { CartProvider } from "./contexts/CartContext";
+import "./assets/images/styles/App.css"; // Estilos globales
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CartProvider>
+      <Router>
+        <div className="App">
+          <Header />
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/products/:id" element={<ProductDetail />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </CartProvider>
   );
 }
 
